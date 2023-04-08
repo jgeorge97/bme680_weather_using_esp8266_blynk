f4b73e951cb11284dcdb2efbc934f965849fc80a
George Jose <jgeorge97@gmail.com>
2023-04-08 16:11:08 +0530

    Removed faulty timer code, added save EEPROM state

--- blynk_weather.ino
+++ blynk_weather.ino
@@ -1,24 +1,36 @@
 #include <ESP8266WiFi.h>
+#include <EEPROM.h>
 #include <BlynkSimpleEsp8266.h>
 #include "bsec.h"
 
 #include "secrets.h"
 
+const uint8_t bsec_config_iaq[] = {
+#include "config/generic_33v_3s_4d/bsec_iaq.txt"
+};
+
+#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000)  // 360 minutes - 4 times a day
+
 // Helper functions declarations
 void checkIaqSensorStatus(void);
 void errLeds(void);
+void loadState(void);
+void updateState(void);
 
 char ssid[] = SSID;
 char pass[] = PASS;
 
 // Create an object of the class Bsec
 Bsec iaqSensor;
+uint8_t bsecState[BSEC_MAX_STATE_BLOB_SIZE] = { 0 };
+uint16_t stateUpdateCounter = 0;
 
 String output;
 
-BlynkTimer timer;
 // Entry point for the example
 void setup(void) {
+  EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);  // 1st address for the length
+
   /* Initializes the Serial communication */
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -29,6 +41,8 @@ void setup(void) {
   iaqSensor.begin(BME68X_I2C_ADDR_LOW, Wire);
   output = "BSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
   Serial.println(output);
+  checkIaqSensorStatus();
+  iaqSensor.setConfig(bsec_config_iaq);
   checkIaqSensorStatus();
 
   bsec_virtual_sensor_t sensorList[13] = {
@@ -53,30 +67,28 @@ void setup(void) {
   // Print the header
   output = "Timestamp [ms], IAQ, IAQ accuracy, Static IAQ, CO2 equivalent, breath VOC equivalent, raw temp[°C], pressure [hPa], raw relative humidity [%], gas [Ohm], Stab Status, run in status, comp temp[°C], comp humidity [%], gas percentage";
   Serial.println(output);
-
-  getSensorValues();
-
-  // Setup a function to be called every 30 seconds
-  timer.setInterval(30000L, getSensorValues);
 }
 
 // Function that is looped forever
 void loop(void) {
   Blynk.run();
-  timer.run();
+  getSensorValues();
 }
 
 void getSensorValues() {
   unsigned long time_trigger = millis();
   if (iaqSensor.run()) {  // If new data is available
-    digitalWrite(LED_BUILTIN, LOW);
-
     Blynk.virtualWrite(V1, iaqSensor.iaq);
     Blynk.virtualWrite(V2, iaqSensor.temperature);
     Blynk.virtualWrite(V3, iaqSensor.humidity);
     Blynk.virtualWrite(V4, iaqSensor.pressure / 100);  //Dividing by 100 to convert Pa to hPa
     Blynk.virtualWrite(V5, iaqSensor.co2Equivalent);
     Blynk.virtualWrite(V6, iaqSensor.breathVocEquivalent);
+    Blynk.virtualWrite(V7, iaqSensor.iaqAccuracy);
+    Blynk.virtualWrite(V8, iaqSensor.stabStatus);
+    Blynk.virtualWrite(V9, iaqSensor.runInStatus);
+    Blynk.virtualWrite(V10, iaqSensor.staticIaq);
+    Blynk.virtualWrite(V11, iaqSensor.gasPercentage);
 
     output = String(time_trigger);
     output += ", " + String(iaqSensor.iaq);
@@ -95,6 +107,8 @@ void getSensorValues() {
     output += ", " + String(iaqSensor.gasPercentage);
     Serial.println(output);
 
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(10);
     digitalWrite(LED_BUILTIN, HIGH);
   } else {
     checkIaqSensorStatus();
@@ -135,6 +149,63 @@ void errLeds(void) {
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
+}
+
+void loadState(void)
+{
+  if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
+    // Existing state in EEPROM
+    Serial.println("Reading state from EEPROM");
+
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
+      bsecState[i] = EEPROM.read(i + 1);
+      Serial.println(bsecState[i], HEX);
+    }
+
+    iaqSensor.setState(bsecState);
+    checkIaqSensorStatus();
+  } else {
+    // Erase the EEPROM with zeroes
+    Serial.println("Erasing EEPROM");
+
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++)
+      EEPROM.write(i, 0);
+
+    EEPROM.commit();
+  }
+}
+
+void updateState(void)
+{
+  bool update = false;
+  if (stateUpdateCounter == 0) {
+    /* First state update when IAQ accuracy is >= 3 */
+    if (iaqSensor.iaqAccuracy >= 3) {
+      update = true;
+      stateUpdateCounter++;
+    }
+  } else {
+    /* Update every STATE_SAVE_PERIOD minutes */
+    if ((stateUpdateCounter * STATE_SAVE_PERIOD) < millis()) {
+      update = true;
+      stateUpdateCounter++;
+    }
+  }
+
+  if (update) {
+    iaqSensor.getState(bsecState);
+    checkIaqSensorStatus();
+
+    Serial.println("Writing state to EEPROM");
+
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE ; i++) {
+      EEPROM.write(i + 1, bsecState[i]);
+      Serial.println(bsecState[i], HEX);
+    }
+
+    EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
+    EEPROM.commit();
+  }
 }
 
 // This function is called every time the device is connected to the Blynk.Cloud
